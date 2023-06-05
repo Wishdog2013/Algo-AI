d4947949c82078ba42c59c907ba8e23e5a8f096f
Wish <wishdog2013@hotmail.com>
2023-06-05 15:03:59 -0500

    question tracking so we can make more answers!

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ function toggleDarkMode() {
 }
 
 function sendMessage() {
-  const inputElement = document.getElementById('user-input');
-  const message = inputElement.value;
-  inputElement.value = '';
+  const nameInput = document.getElementById('name-input');
+  const emailInput = document.getElementById('email-input');
+  const messageInput = document.getElementById('user-input');
+
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const message = messageInput.value;
+
+  nameInput.value = '';
+  emailInput.value = '';
+  messageInput.value = '';
+
   displayMessage('User', message);
+
+  // Collect data and send to Discord webhook
+  sendToWebhook(name, email, message);
+
   const output = simpleAI(message);
   displayMessage('Algo', output);
 }
@@ -30,8 +43,32 @@ function simpleAI(input) {
     "I'm sorry, I don't have the answer to that.",
     "Let me think about it for a moment.",
     "I appreciate your input.",
-    "Make an issue to add a response if you'd like to!"
+    "make a issue to add a response if you'd like to!"
   ];
   const randomIndex = Math.floor(Math.random() * responses.length);
   return responses[randomIndex];
+}
+
+function sendToWebhook(name, email, message) {
+  const webhookUrl = "https://discord.com/api/webhooks/1115368177979904140/zfci8BYck_FyrFtm-MZ_4-ot3WiDvXJg7gcFDG0xyUka7EcAIYLu6RgdaYVD-Fnbtvog";
+
+  const requestData = {
+    username: "Algo-AI",
+    avatar_url: "https://cdn.discordapp.com/attachments/1115370154642124961/1115370214859739146/8t4kj4LS_400x400_1.jpg", // Optional: Customize the bot's avatar
+    content: `New Message:\nName: ${name}\nEmail: ${email}\nMessage: ${message}`
+  };
+
+  fetch(webhookUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(requestData)
+  })
+    .then(function(response) {
+      console.log("Message sent to Discord webhook:", response);
+    })
+    .catch(function(error) {
+      console.error("Failed to send message to Discord webhook:", error);
+    });
 }
