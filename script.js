c2e10443cfb99df0a80cd18172c84a93506cb3ab
Wish <wishdog2013@hotmail.com>
2023-06-04 20:21:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 // Define a function that takes an input and returns an output
 function simpleAI(input) {
   // Define your algorithm logic here
-  if (input === 'hello') {
+  if (input.toLowerCase().includes('hello')) {
     return 'Hi there!';
-  } else if (input === 'bye') {
+  } else if (input.toLowerCase().includes('how are you')) {
+    return 'I am an AI, so I do not have feelings, but thanks for asking!';
+  } else if (input.toLowerCase().includes('bye')) {
     return 'Goodbye!';
   } else {
-    return 'I do not understand.';
+    return 'I am sorry, I did not understand that.';
   }
 }
 
 // Get user input and interact with the AI
-function getUserInput() {
-  const input = prompt('Enter your message:');
+function chatWithAI() {
+  const input = prompt('You: ');
   const output = simpleAI(input);
-  alert(output);
+  displayMessage('AI:', output);
+}
+
+// Display the message in the chat window
+function displayMessage(sender, message) {
+  const chatContainer = document.getElementById('chat-container');
+  const messageElement = document.createElement('div');
+  messageElement.innerHTML = `<strong>${sender}</strong>: ${message}`;
+  chatContainer.appendChild(messageElement);
 }
 
 // Call the function to start the interaction
-getUserInput();
+chatWithAI();
